c7e61864b979eb5352b9b636988558e40dac940a
saladbowl0123 <saladbowl0123@gmail.com>
2024-05-29 00:18:50 +0800

    09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let clean = myString
+    .toLowerCase()
+    .split("")
+    .filter(char => alphanumerical.includes(char))
+    .join("");
+  let reverse = clean
+    .split("")
+    .reverse()
+    .join("");
+  return clean == reverse;
 };
 
 // Do not edit below this line
