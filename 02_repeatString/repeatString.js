67b120d189f131647153b1315913ef0d23877c19
saladbowl0123 <saladbowl0123@gmail.com>
2024-05-28 15:16:01 +0800

    02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(myString, numReps) {
+  if (numReps < 0) return "ERROR";
+  let r = "";
+  for (let i = 0; i < numReps; i++) {
+    r += myString;
+  }
+  return r;
 };
 
 // Do not edit below this line
