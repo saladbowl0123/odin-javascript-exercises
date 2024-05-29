f2dbfdebd479a1518bb4fa454de726667d7abf92
saladbowl0123 <saladbowl0123@gmail.com>
2024-05-29 10:51:23 +0800

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  // switch cases use strict comparison `===`, so convert string to int
+  num = parseInt(num);
+  if (num < 0) return "OOPS";
+  switch (num) {
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    default:
+      return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 };
 
 // Do not edit below this line
