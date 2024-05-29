2737355f91dd4cdba698a9af0cd5d9a865a7f9c8
saladbowl0123 <saladbowl0123@gmail.com>
2024-05-29 22:00:02 +0800

    12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
 
+  const getAge = function (person) {
+    person.yearOfDeath ??= new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+  }
+  
+  return people.reduce((oldest, person) =>
+    getAge(oldest) < getAge(person) ? person : oldest);
 };
 
 // Do not edit below this line
